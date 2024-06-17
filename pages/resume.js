4265b4f783a01d4a5789596fc3e6d5b660379a41
Sirryan2002 <80364400+Sirryan2002@users.noreply.github.com>
2024-06-16 23:53:43 -0400

    significant resume page improvements

--- pages/resume.js
+++ pages/resume.js
@@ -8,73 +8,117 @@ const Resume = () => {
       <Head>
         <title>Resume - Ryan Longo</title>
         <meta name="description" content="Resume of Ryan Longo" />
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' />
       </Head>
       <NavBar />
       <section id="resume" className="section resume">
-        <h2>My Resume</h2>
-        <div className="resume-content">
-          <h3>Ryan Longo</h3>
-          <p>129 Stoddard Ave, East Lansing • (616) 824-1182 • longorya@msu.edu</p>
-          
-          <h3>Education</h3>
-          <p><strong>Michigan State University, James Madison College</strong> - December 2024</p>
-          <ul>
-            <li>B.A. Social Relations & Policy - GPA 3.76/4.00</li>
-            <li>B.A. Economics - GPA 3.91/4.00</li>
-            <li>Specialization in Educational Studies & Policy</li>
-          </ul>
+        <div className="resume-container">
+          <div className="header">
+            <i className='bx bxs-user header-icon'></i>
+            <h1>My Resume</h1>
+          </div>
+          <div className="resume-content">
+            <h3>Education</h3>
+            <div className="experience">
+              <img src="/spartan-logo.png" alt="Michigan State University Logo" className="company-logo" />
+              <div>
+                <p><strong>Michigan State University, James Madison College</strong> - December 2024</p>
+                <ul>
+                  <li>B.A. Social Relations & Policy - GPA 3.76/4.00</li>
+                  <li>B.A. Economics - GPA 3.91/4.00</li>
+                  <li>Specialization in Educational Studies & Policy</li>
+                </ul>
+              </div>
+            </div>
 
-          <h3>Professional Experience</h3>
-          <div className="experience">
-            <img src="/msumun-logo.png" alt="MSU Model United Nations 501c3" className="company-logo" />
-            <div>
-              <p><strong>MSU Model United Nations 501c3</strong> - September 2021-Present</p>
-              <ul>
-                <li>Lead a 501c3 non-profit of 95 employees to provide education on international policy writing and public speaking skills to midwestern middle and high school students.</li>
-                <li>Coordinate an executive board to organize logistics and advertising of 2 conferences.</li>
-                <li>Redesigned the organization's website and implemented new financial controls in-line with IRS 501(c)(3) regulations to return the organization to a positive net income.</li>
+            <h3>Professional Experience</h3>
+            <div className="experience">
+              <img src="/msumun-logo.png" alt="MSU Model United Nations 501c3" className="company-logo" />
+              <div>
+                <p><strong>MSU Model United Nations 501c3</strong> - September 2021-Present</p>
+                <p><em>Secretary-General 2024-2025</em></p>
+                <p><em>USG of Technology 2023-2024</em></p>
+                <ul>
+                  <li>Led a 501c3 non-profit of 95 employees.</li>
+                  <li>Coordinated logistics and advertising for 2 conferences.</li>
+                  <li>Redesigned the organization's website and implemented new financial controls.</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="experience">
+              <img src="/cz-logo.webp" alt="Comfort Zone Cigar Lounge & Bistro" className="company-logo" />
+              <div>
+                <p><strong>Comfort Zone Cigar Lounge & Bistro</strong> - October 2023-Present</p>
+                <p><em>Cocktial Server/Waiter</em></p>
+                <ul>
+                  <li>Engaged guests with personalized service.</li>
+                  <li>Drove sales through expert recommendations and upselling.</li>
+                  <li>Promoted house cocktails, liquors, and wine selections.</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="experience">
+              <img src="/chipotle-logo.png" alt="Chipotle Mexican Grill" className="company-logo" />
+              <div>
+                <p><strong>Chipotle Mexican Grill</strong> - April 2022-October 2023</p>
+                <p><em>Kitchen Leader</em></p>
+                <ul>
+                  <li>Led kitchen operations, ensuring food quality and safety.</li>
+                  <li>Trained team members in kitchen maintenance tasks.</li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="experience">
+              <img src="/upwork-logo.png" alt="Upwork Logo" className="company-logo" />
+              <div>
+                <p><strong>Web-Scraping & Data Formatting Freelancing</strong> - 2023-Present</p>
+                <p><em>Freelancer</em></p>
+                <ul>
+                  <li>Completed web-scraping projects using Python and BeautifulSoup.</li>
+                  <li>Formatted data for clients on Upwork and Fiverr.</li>
+                  <li>Delivered high-quality data analysis and reporting.</li>
+                </ul>
+              </div>
+            </div>
+
+            <h3>Skills & Certifications</h3>
+            <div className="skills-section">
+              <h4>Programming Languages & Tools</h4>
+              <ul className="skills-list">
+                <li><i className='bx bxl-html5'></i> HTML5</li>
+                <li><i className='bx bxl-css3'></i> CSS3</li>
+                <li><i className='bx bxl-javascript'></i> JavaScript</li>
+                <li><i className='bx bxl-react'></i> ReactJS</li>
+                <li><i className='bx bxl-nextjs'></i> NextJS</li>
+                <li><i className='bx bxl-c-plus-plus'></i> C++</li>
+                <li><i className='bx bx-data'></i> MySQL</li>
+                <li><i className='bx bxl-python'></i> Python</li>
+                <li><i className='bx bxs-objects-vertical-bottom'></i> R</li>
+              </ul>
+
+              <h4>Software Skills</h4>
+              <ul className="skills-list">
+                <li><i className='bx bxl-microsoft'></i> Excel</li>
+                <li><i className='bx bxl-microsoft'></i> Teams</li>
+                <li><i className='bx bxl-microsoft'></i> Word</li>
+                <li><i className='bx bxl-google'></i> Google Suite</li>
+                <li><i className='bx bxl-wordpress'></i> Wordpress</li>
+                <li><i className='bx bxl-squarespace'></i> Squarespace</li>
               </ul>
             </div>
           </div>
-
-          <div className="experience">
-            <img src="/cz-logo.webp" alt="Comfort Zone Cigar Lounge & Bistro" className="company-logo" />
-            <div>
-              <p><strong>Comfort Zone Cigar Lounge & Bistro</strong> - October 2023-Present</p>
-              <ul>
-                <li>Engage guests with personalized service, memorizing names and preferences to foster a welcoming atmosphere.</li>
-                <li>Drive sales by expertly recommending menu items, upselling products, and suggesting drink pairings tailored to individual tastes.</li>
-                <li>Cultivate loyalty by promoting house cocktails, liquors, and wine selections, resulting in increased revenue and guest satisfaction.</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="experience">
-            <img src="/chipotle-logo.png" alt="Chipotle Mexican Grill" className="company-logo" />
-            <div>
-              <p><strong>Chipotle Mexican Grill</strong> - April 2022-October 2023</p>
-              <ul>
-                <li>Lead kitchen operations, ensuring strict adherence to food quality standards, ingredient safety, and sanitation protocols, consistently achieving high ecosure audit scores.</li>
-                <li>Exhibited leadership by guiding and training team members in maintaining kitchen cleanliness and completing long-term kitchen maintenance tasks.</li>
-              </ul>
-            </div>
-          </div>
-
-          <h3>Skills & Certifications</h3>
-          <ul>
-            <li>2 Years Data Science Experience; Proficient in R-Studio, Python, SQL, and RDMS’s</li>
-            <li>Proficient in Microsoft Office Products such as Excel, Word, Powerpoint, & Teams</li>
-            <li>Experience in Graphic & Website Design; Proficient with Squarespace, Weebly, Javascript, ReactJS, HTML, and CSS based coding languages</li>
-          </ul>
+          <h3>Resume PDF</h3>
+          <iframe 
+            src="/resume.pdf" 
+            width="100%" 
+            height="1000px" 
+            style={{ border: 'none' }}
+            title="Ryan Longo Resume"
+          ></iframe>
         </div>
-        <h3>Resume PDF</h3>
-        <iframe 
-          src="/resume.pdf" 
-          width="100%" 
-          height="1000px" 
-          style={{ border: 'none' }}
-          title="Ryan Longo Resume"
-        ></iframe>
       </section>
       <Footer />
     </div>
