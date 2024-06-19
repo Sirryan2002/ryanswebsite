7dd17c7ef40e433a2202600fb6555ed3f09eecc1
Sirryan2002 <80364400+Sirryan2002@users.noreply.github.com>
2024-06-19 16:39:17 -0400

    oops

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 // index.js
 import { useEffect } from 'react';
+import Head from 'next/head';
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
