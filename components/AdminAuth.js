74c79ad99c5a8a6d6965072fcf65bd9a78d6323e
Sirryan2002 <80364400+Sirryan2002@users.noreply.github.com>
2024-07-01 21:00:11 -0400

    Revert "Revert "some testing""
    This reverts commit b7752733b8b7010b5703d5a9ae9a0f0d3af74072.

--- components/AdminAuth.js
+++ components/AdminAuth.js
@@ -7,6 +7,7 @@ const AdminAuth = ({ onAuthSuccess }) => {
 
   const handleLogin = () => {
     if (password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
+        
       onAuthSuccess();
     } else {
       setError('Incorrect password');
