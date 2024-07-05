7bc7ea423d2ef53a20960625bc1af1051cc6317f
Sirryan2002 <80364400+Sirryan2002@users.noreply.github.com>
2024-07-05 00:23:30 -0400

    Makes the Navbar Mobile Friendly and Responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,36 @@
+import { useState } from 'react';
+
 import Link from 'next/link'; // Import Link from next.js
 import Image from 'next/image';
 
+import initialsPNG from '../public/initials.png';
+
 function NavBar({props, context}) {
-    return (
-        <header id='NavBar'>
-        <Link href="/">
-            <img src={'/initials.png'} alt="Initials" className='initials'/>
-        </Link>
-        <nav>
-            <Link href="/" className="Header-nav-item">Home</Link> {/* Link to projects.js */}
-            <Link href="/resume" className="Header-nav-item">Resume</Link> {/* Link to projects.js */}
-            <Link href="/portfolio" className="Header-nav-item">Portfolio</Link>
-            <Link href="/services" className="Header-nav-item">Services</Link>
-            <Link href="/#contact" className="Header-nav-item">Contact</Link>
-        </nav>
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+      setIsNavOpen(!isNavOpen);
+  };
+
+  return (
+      <header id='NavBar'>
+          <div className='logo-button-container'>
+              <Link href="/" className='initials-container'>
+                  <Image src={initialsPNG} alt="Initials" className='initials'/>
+              </Link>
+              <button className="toggle-button" onClick={toggleNav}>
+                  ☰
+              </button>
+          </div>
+          <nav className={isNavOpen ? 'active' : ''}>
+              <Link href="/" className="Header-nav-item">Home</Link>
+              <Link href="/resume" className="Header-nav-item">Resume</Link>
+              <Link href="/portfolio" className="Header-nav-item">Portfolio</Link>
+              <Link href="/services" className="Header-nav-item">Services</Link>
+              <Link href="/#contact" className="Header-nav-item">Contact</Link>
+          </nav>
       </header>
-    )
-  }
+  )
+};
 
 export default NavBar;
